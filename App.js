08816d1340e978374a86807ffad26bc3b52a18bf
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-07-09 16:01:36 -0600

    implement basic text input and text display

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class HelloWorld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
       {/* Create parent view to hold 3 sections use flexDirection: column to display the view's children on top of eachother */ },
       <View style={[styles.container, styles.pushOut]}>
         <View style={[styles.box1, styles.cornerBox]}></View>
         <View style={styles.box2}></View>
+        <View style={styles.box4}>
+          <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Text style={styles.textColor}>You Wrote: {this.state.text}</Text>
+            <TextInput
+              style={{ height: 40, borderColor: 'grey', color: 'white', borderWidth: 1 }}
+              onChangeText={(text) => this.setState({ text })}
+              value={this.state.text}
+              placeholder='Type here ...'
+            />
+          </View>
+        </View>
         <View style={[styles.box3, styles.bottomBox]}></View>
       </View>
     );
@@ -31,15 +47,17 @@ const styles = StyleSheet.create({
   box3: {
     backgroundColor: 'purple'
   },
+  box4: {
+    flex: 1,
+    backgroundColor: 'black'
+  },
   cornerBox: {
     height: 60,
   },
   bottomBox: {
-    width: 100,
-    height: 200
+    height: 280
   },
-  pushOut: {
-    marginLeft: 5,
-    marginRight: 5
+  textColor: {
+    color: 'white'
   },
 });
