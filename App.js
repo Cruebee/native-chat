4eb55aef49966f3164fb7596050c0fe747443848
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-07-09 14:59:55 -0600

    adjusting some layout feature capabilities with flexbox

--- App.js
+++ App.js
@@ -1,50 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.blue}>Hello World!!</Text>
-      <Text style={styles.bigRed}>How Are You?</Text>
-      <Text style={[styles.blue, styles.bigRed]}>I'm Feeling Blue-ish!</Text>
-      {/* Creating a layout with Flexbox */}
-      <View style={styles.box}></View>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      {/* Create parent view to hold 3 sections use flexDirection: column to display the view's children on top of eachother */ },
+      <View style={[styles.container, styles.pushOut]}>
+        <View style={[styles.box1, styles.cornerBox]}></View>
+        <View style={styles.box2}></View>
+        <View style={[styles.box3, styles.bottomBox]}></View>
+      </View>
+    );
+  }
 }
-
-// Remove excess code since you created the styles needed with two different style objects (blue and bigRed)
-// <Text style={styles.bigRedBold}>I'm Feeling Blue-ish!</Text>
-// Removed bigRedBold style since you can pass the styles needed from both blue and bigRed with an array in the style prop
-/* bigRedBold: {
-  color: 'red',
-  fontSize: 30,
-  fontWeight: '600',
-},
-*/
-// The bigRed style in the third <Text> takes precidence as it's last in the array.
-// The fontWeight from blue is carried over since bigRed doesn't specify that value.
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'orange'
   },
-  blue: {
-    color: 'blue', // Colors can also be defined as HEX codes.
-    fontWeight: '600', // 600 defines a BOLD font
+  box1: {
+    flex: 0.6,
+    backgroundColor: 'blue'
   },
-  bigRed: {
-    color: 'red',
-    fontSize: 30,
+  box2: {
+    flex: 5,
+    backgroundColor: 'green'
   },
-  box: {
-    width: 60,
+  box3: {
+    backgroundColor: 'purple'
+  },
+  cornerBox: {
     height: 60,
-    backgroundColor: 'blue',
+  },
+  bottomBox: {
+    width: 100,
+    height: 200
+  },
+  pushOut: {
+    marginLeft: 5,
+    marginRight: 5
   },
 });
