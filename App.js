d432951decb1a8bf08de46c5e7cd0f17f9a28bd7
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-07-09 14:14:22 -0600

    first commit-create basic React-Native app, explore some basic features. Comments and code within app.js to be removed after second commit.

--- App.js
+++ App.js
@@ -5,11 +5,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.blue}>Hello World!!</Text>
+      <Text style={styles.bigRed}>How Are You?</Text>
+      <Text style={[styles.blue, styles.bigRed]}>I'm Feeling Blue-ish!</Text>
+      {/* Creating a layout with Flexbox */}
+      <View style={styles.box}></View>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+// Remove excess code since you created the styles needed with two different style objects (blue and bigRed)
+// <Text style={styles.bigRedBold}>I'm Feeling Blue-ish!</Text>
+// Removed bigRedBold style since you can pass the styles needed from both blue and bigRed with an array in the style prop
+/* bigRedBold: {
+  color: 'red',
+  fontSize: 30,
+  fontWeight: '600',
+},
+*/
+// The bigRed style in the third <Text> takes precidence as it's last in the array.
+// The fontWeight from blue is carried over since bigRed doesn't specify that value.
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +33,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  blue: {
+    color: 'blue', // Colors can also be defined as HEX codes.
+    fontWeight: '600', // 600 defines a BOLD font
+  },
+  bigRed: {
+    color: 'red',
+    fontSize: 30,
+  },
+  box: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'blue',
   },
 });
