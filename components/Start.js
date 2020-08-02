2ceb8ca2f6d7a8988ea493faea2f3b6247d67351
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-08-02 15:09:29 -0600

    Revert "5.4 submitted, fixed some inconsistencies and stopped app from crashing when messages are sent"
    This reverts commit ade16329c72f70145ad55fe8ee29f794af808a95.

--- components/Start.js
+++ components/Start.js
@@ -10,10 +10,6 @@ import {
 } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-// Import Firestore
-const firebase = require('firebase');
-require('firebase/firestore');
-
 export default class Start extends React.Component {
   constructor(props) {
     super(props);
