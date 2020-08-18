2149ea50004366b7b4c95f6b0db2feaf97d3bebe
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-08-18 11:00:28 -0600

    begin linting, prior to branch addition.

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import Chat from './components/Chat';
-import Start from './components/Start';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-console.ignoredYellowBox = ['setting a timer'];
+import Chat from './components/Chat';
+import Start from './components/Start';
+
+console.ignoredYellowBox = ['setting a timer']; // eslint-disable-line no-console
 
 const navigator = createStackNavigator({
   Start: {
